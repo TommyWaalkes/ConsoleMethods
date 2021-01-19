8080828d6808e3bfc4ebdd7d548edecb012e5b90
Tommy Waalkes <Tommy@grandcircus.co>
2021-01-19 11:51:19 -0500

    Intial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Please input a valid int");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            Console.WriteLine(num);
+            
+        }
+    }
+}
